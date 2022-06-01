ed9ddd82ca155b4f5a3c4b924427afb560e7e154
Diego Parra <28814932+diegoapb@users.noreply.github.com>
2022-06-01 01:01:59 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const HomePage: NextPage = () => {
+    return (
+        <>
+            <h1>HolaMundo</h1>
+        </>
+    );
+};
+
+export default HomePage;
