b776015107f20b8db3d1dd4238b4c834cd72275f
Diego Parra <28814932+diegoapb@users.noreply.github.com>
2022-06-01 21:11:00 -0500

    config _document file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
     return (
         <>
-            <h1>HolaMundo</h1>
+            <Button color={"gradient"}>HolaMundo</Button>
         </>
     );
 };
